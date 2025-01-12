44f0b6bd47becf88c6e22c9fff04ec4741f81c17
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-01-12 17:01:08 -0300

    feat: implement SimilarExercises component in ExerciseDetail

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -92,9 +92,9 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 			<Box sx={{ position: "relative", width: "100%", p: "20px" }}>
 				<HorizontalScrollbar
 					data={bodyParts}
-					bodyParts
 					setBodyPart={setBodyPart}
 					bodyPart={bodyPart}
+					bodyParts
 				/>
 			</Box>
 		</Stack>
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -10,6 +10,8 @@ import SimilarExercises from "../components/SimilarExercises";
 const ExerciseDetail = () => {
 	const [exerciseDetail, setExerciseDetail] = useState({});
 	const [exerciseVideos, setExerciseVideos] = useState([]);
+	const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+	const [equipmentExercises, setEquipmentExercises] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
@@ -30,6 +32,20 @@ const ExerciseDetail = () => {
 				youtubeOptions
 			);
 			setExerciseVideos(exerciseVideosData.contents);
+
+			const targetMuscleExercisesData = await fetchData(
+				`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+				exerciseOptions,
+				30
+			);
+			setTargetMuscleExercises(targetMuscleExercisesData);
+
+			const equipmentExercisesData = await fetchData(
+				`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
+				exerciseOptions,
+				30
+			);
+			setEquipmentExercises(equipmentExercisesData);
 		};
 
 		fetchExercisesData();
@@ -42,7 +58,10 @@ const ExerciseDetail = () => {
 				exerciseVideos={exerciseVideos}
 				name={exerciseDetail.name}
 			/>
-			<SimilarExercises />
+			<SimilarExercises
+				targetMuscleExercises={targetMuscleExercises}
+				equipmentExercises={equipmentExercises}
+			/>
 		</Box>
 	);
 };
